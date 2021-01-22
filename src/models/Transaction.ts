f28ac08613ede05d8e1e4df76bd7d3294be61be9
Luiz Eduardo Franco <60003877+lEduFranco@users.noreply.github.com>
2021-01-22 16:17:55 -0300

    Initial commit

---
+++ src/models/Transaction.ts
@@ -0,0 +1,20 @@
+import { v4 as uuid } from 'uuid';
+
+class Transaction {
+  id: string;
+
+  title: string;
+
+  value: number;
+
+  type: 'income' | 'outcome';
+
+  constructor({ title, value, type }: Omit<Transaction, 'id'>) {
+    this.id = uuid();
+    this.title = title;
+    this.value = value;
+    this.type = type;
+  }
+}
+
+export default Transaction;
